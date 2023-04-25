f56194c3a68f999c9ee868a86159c42f2ffabf9f
Mattéo <matteo.fauvel@viacesi.fr>
2023-04-25 11:24:53 +0200

    change 2

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <link rel="stylesheet" href="/style/mainStyle.css" />
+  <link rel="icon" href="/assets/Images/site-icon.png" type="image/png" />
+
+  <title>Home - Portfolio</title>
+</head>
+
+<body>
+  <header>
+    <?php
+    require ("/Pages/navbar.html")
+  ?>
+  </header>
+
+  <!-- <main>
+    <button>Click</button>
+  </main>
+
+  <footer>
+    <a href="https://www.flaticon.com/" title="Bibliothèques d'icônes">Bibliothèques d'icônes</a>
+  </footer> -->
+</body>
+
+</html>
