45d17b2507eaaa1c5a49d36faa7548a5c07fe7e3
Mattéo <matteo.fauvel@viacesi.fr>
2023-05-09 14:12:10 +0200

    ef

--- Script/script.js
+++ Script/script.js
@@ -5,12 +5,10 @@ changeLanguage("lang-fr");
 function changeLanguage(language){
 
   if (language == "lang-fr"){
-    // document.getElementsByClassName("lang-en").style.display = "none";
     $(".lang-en").hide();
     $("#lang_button").text("lang-en");
   }
   else if (language == "lang-en"){
-    // document.getElementsByClassName("lang-fr").style.display = "none";
     $(".lang-fr").hide();
     $("#lang_button").text("lang-fr")
   }
